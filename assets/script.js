64a59c4aab9e8f7e9ad8e129ce0e6c2e3014a813
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-02-09 11:27:23 -0500

    clear history button now has functionality

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,20 @@
-const apiKey = "sHxGwmiK3oD1A33jienk7QUloE04aUXJcYj9OezNWuwdDq4Auf";
+// const apiKey = "sHxGwmiK3oD1A33jienk7QUloE04aUXJcYj9OezNWuwdDq4Auf";
 
-const apiSecret = "iilHX2WeaB32z0UiDuGCUblsqUZgzMhJPkBEZEUh";
-window.FavoriteStarElement = document.registerElement(
-    'favorite-star', 
-    { prototype: proto }
-);
-    var saveBtn= document.getElementById("saveDog");
+// const apiSecret = "iilHX2WeaB32z0UiDuGCUblsqUZgzMhJPkBEZEUh";
+// window.FavoriteStarElement = document.registerElement(
+//     'favorite-star', 
+//     { prototype: proto }
+// );
+var saveBtn = document.getElementById("saveDog");
 
-    saveBtn.addEventListener("click",myFunction); 
-    function myFunction()
-    {
-        document.getElementById("saved-list").innerHTML = "hey";
-  }
+saveBtn.addEventListener("click", myFunction);
+function myFunction() {
+    document.getElementById("saved-list").innerHTML = "hey";
+}
 
+var clearBtn = document.getElementById("clear");
+
+clearBtn.addEventListener("click", clearHistory)
+function clearHistory() {
+    document.getElementById("saved-list").innerHTML = ""
+}
