2e70c07ba839806d30742386a630846f0a82bdff
Franklyn Tavarez <kurbby.450@gmail.com>
2022-02-08 16:34:44 -0800

    adding favorite star

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
 const apiKey = "sHxGwmiK3oD1A33jienk7QUloE04aUXJcYj9OezNWuwdDq4Auf";
 
 const apiSecret = "iilHX2WeaB32z0UiDuGCUblsqUZgzMhJPkBEZEUh";
+window.FavoriteStarElement = document.registerElement(
+    'favorite-star', 
+    { prototype: proto }
+);
+var proto = Object.create(HTMLElement.prototype);
