dc5c663dbe3e27d907ea2c67ffc9de0811233456
Franklyn Tavarez <kurbby.450@gmail.com>
2022-02-10 11:32:22 -0800

    changes to js file

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 getJSON( )
 const apiKey = "sHxGwmiK3oD1A33jienk7QUloE04aUXJcYj9OezNWuwdDq4Auf";
 
 
 const apiSecret = "iilHX2WeaB32z0UiDuGCUblsqUZgzMhJPkBEZEUh";
-window.FavoriteStarElement = document.registerElement(
-    'favorite-star', 
-    { prototype: proto }
-);
     var saveBtn= document.getElementById("saveDog");
 
     saveBtn.addEventListener("click",myFunction); 
@@ -15,6 +10,4 @@ window.FavoriteStarElement = document.registerElement(
     {
         document.getElementById("saved-list").innerHTML = "hey";
   }
-
-=======
->>>>>>> 7fcb158de0574a55235667aaecc7d0286e5137fc
+ // 7fcb158de0574a55235667aaecc7d0286e5137fc
