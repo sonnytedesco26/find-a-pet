3ca6bccdcef59f360a049c0bb9ad86f3626f3274
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-02-09 20:50:48 -0500

    Got API connection with picture to function with search button

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,51 @@
-// const apiKey = "sHxGwmiK3oD1A33jienk7QUloE04aUXJcYj9OezNWuwdDq4Auf";
 
-// const apiSecret = "iilHX2WeaB32z0UiDuGCUblsqUZgzMhJPkBEZEUh";
 // window.FavoriteStarElement = document.registerElement(
 //     'favorite-star', 
 //     { prototype: proto }
 // );
-var saveBtn = document.getElementById("saveDog");
+
+var dogArray = [];
+
+var saveBtn = document.getElementById("saveDog");;
+var clearBtn = document.getElementById("clear");
+var searchBtn = document.getElementById("search");
+
+searchBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    var userInput = document.getElementById("breedInput").value;
+    fetch(`https://dog.ceo/api/breed/${userInput}/images`)
+    .then(function(response) {return response.json()})
+    .then(data => console.log(data));
+    console.log(userInput);
+})
 
 saveBtn.addEventListener("click", myFunction);
 function myFunction() {
     document.getElementById("saved-list").innerHTML = "hey";
 }
 
-var clearBtn = document.getElementById("clear");
-
 clearBtn.addEventListener("click", clearHistory)
 function clearHistory() {
     document.getElementById("saved-list").innerHTML = ""
+}
+
+
+
+function displayFinder(data){
+
+}
+
+function loadListofBreeds(){
+    fetch("https://dog.ceo/api/breeds/list/all")
+    
+    .then(function(response) {return response.json()})
+    .then(data => console.log(data));
+}
+
+function getDog(){
+
+    fetch(`https://dog.ceo/api/${userInput}/image/random`)
+    .then(function(response) {return response.json()})
+    .then(data => console.log(data));
+    console.log(userInput);
 }
