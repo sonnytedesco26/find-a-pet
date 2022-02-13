323cae2b64a03d3ebbe200b3329dc11cc679e281
Franklyn Tavarez <kurbby.450@gmail.com>
2022-02-12 20:19:55 -0800

    api issues and new branch

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 const apiSecret = 'iilHX2WeaB32z0UiDuGCUblsqUZgzMhJPkBEZEUh'
+const apiKey = 'sHxGwmiK3oD1A33jienk7QUloE04aUXJcYj9OezNWuwdDq4Auf'
 var dogArray = [];
 var saveBtn = document.getElementById("saveDog");;
 var clearBtn = document.getElementById("clear");
@@ -8,10 +9,10 @@ var image = document.getElementById("image")
 function picturetest(e){
     e.preventDefault();
     var userInput = document.getElementById("breedInput").value;
-    fetch(`https://dog.ceo/api/breed/${userInput}/images`)
-        .then(function (response) { return response.json() })
-        .then(data => console.log(data));
-    console.log(userInput);
+    // fetch(`https://dog.ceo/api/breed/${userInput}/images`)
+    //     .then(function (response) { return response.json() })
+    //     .then(data => console.log(data));
+    // console.log(userInput);
     fetch("https://api.petfinder.com/v2/oauth2/token", {
         method: "POST",
         body: "grant_type=client_credentials&client_id=" + apiKey + "&client_secret=" + apiSecret,
@@ -22,7 +23,7 @@ function picturetest(e){
         return response.json();
     }).then(function(data){
         console.log('token', data)
-        return fetch('https://api.petfinder.com/v2/animals?' + searchBtn ,{
+        return fetch('https://api.petfinder.com/v2/animals?location=' + userInput ,{
             headers: {
                 'Authorization': data.token_type + ' ' + data.access_token,
                 'Content-Type': 'application/x-www-form-urlencoded'
@@ -57,30 +58,60 @@ function getDog() {
         .then(data => console.log(data));
     console.log(userInput);
 }
-searchBtn.addEventListener('click', function(){
-    var userInput = document.getElementById("breedInput").value;
-    fetch(`https://dog.ceo/api/breed/${userInput}/images`)
-        .then(res => res.json())
-        .then(result => {
-            console.log(result)
-            image.setAttribute( "src", result.message[0])
-        })
-        .catch(err=>console.log(err))
+searchBtn.addEventListener('click', function(e){
+     picturetest(e)
+    // var userInput = document.getElementById("breedInput").value;
+    // fetch(`https://dog.ceo/api/breed/${userInput}/images`)
+    //     .then(res => res.json())
+    //     .then(result => {
+    //         console.log(result)
+    //         image.setAttribute( "src", result.message[0])
+    //     })
+    //     .catch(err=>console.log(err))
 })
 
 $('.addToFavBtn').on('click', event => {
-    var storeName = $(event.target).parents()[1].firstElementChild.innerHTML;
+  var storeName = $(event.target).parents()[1].firstElementChild.innerHTML;
+
+  var favorites = localStorage.getItem("favorites");
+  if (!favorites){
+    localStorage.setItem("favorites", JSON.stringify({stores:[]}));
+    favorites = JSON.parse(localStorage.getItem("favorites"));
+  }else{
+    favorites = JSON.parse(favorites);
+  }
   
-    var favorites = localStorage.getItem("favorites");
-    if (!favorites){
-      localStorage.setItem("favorites", JSON.stringify({stores:[0]}));
-      favorites = JSON.parse(localStorage.getItem("favorites"));
-    }else{
-      favorites = JSON.parse(favorites);
-    }
-    
-    favorites.stores.push(storeName);
-  
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-  
-  });
+  favorites.stores.push(storeName);
+
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+
+//   var todos =JASON.parse(localStorage.getItem("todos")) || []
+//   var list = $("#list")
+//   funtion appendTodo(todos){
+// //$.each(todos, funtion(i, todo){
+//   var le = $("<li>")
+//       li.text(todo.title)
+//       list.append(li)
+      
+//       funtion appenTodo()todos{
+//           var li = $("<li>")
+//               li.text(todo)
+//               li.attr("data-index", i)
+//               list,
+//               .append(li)
+//       }
+
+//       list.on("click", "li", funtion(e){
+//           var clickedItem = $(e.target)
+//           todos.splice(click.attr("datda-index") 1)
+//           localStorage.setIyem("todos", JSOM.strinify(todos))
+
+//           clickedItem.remove()
+          
+//           $("submit-btn").on("lick", funtion)
+//           todo.push("#todo-input").val())
+//           localStorage.setItem("todos", JSON.strinify(todos))
+//           appendTodo(todo)
+//       }
+// } 
+});
