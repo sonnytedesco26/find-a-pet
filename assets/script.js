ebc64d0c74107e302082c47c39c9b3577c7adf24
Xhemajl Camaj <Xhemajlc@Agrons-MacBook-Pro-2.local>
2022-02-12 21:15:05 -0500

    random fact generator

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 const apiKey = 'sHxGwmiK3oD1A33jienk7QUloE04aUXJcYj9OezNWuwdDq4Auf'
 const apiSecret = 'iilHX2WeaB32z0UiDuGCUblsqUZgzMhJPkBEZEUh'
 
-var dogArray = [];
+const dogArray = ['It is a myth that dogs are color blind. They can actually see in color, just not as vividly as humans. It is akin to our vision at dusk.', 'If never spayed or neutered, a female dog, her mate, and their puppies could produce over 66,000 dogs in 6 years!',
+    'A one year old dog is as mature, physically, as a 15 year old human.', 'An American Animal Hospital Assoc. poll found that 33% of dog owners admit to talking to their dogs on the phone and leaving answering machine messages for them while away.', "Dog's nose prints are as unique as a human's finger prints and can be used to accurately identify them.",
+    'Dogs have no sense of “time”.', "Every dog on earth likely descended from a species knows as the Tomarctus, a creature that roamed the earth over 15 million years ago.",];
 
 var saveBtn = document.getElementById("saveDog");;
 var clearBtn = document.getElementById("clear");
 var searchBtn = document.getElementById("search");
+var factBtn = document.getElementById('fact-btn');
+
+
 
 
 
@@ -39,7 +44,7 @@ searchBtn.addEventListener("click", function (e) {
         }).then(function (data) {
             console.log(userInput, data)
             return `<p>Name: </p> ${data.name}`
-            
+
 
         })
 
@@ -50,6 +55,12 @@ searchBtn.addEventListener("click", function (e) {
 
 });
 
+factBtn.addEventListener('click', createFact);
+function createFact() {
+    document.getElementById("dogfact").innerHTML =
+        dogArray[Math.floor(Math.random() * dogArray.length)];
+}
+
 
 saveBtn.addEventListener("click", myFunction);
 function myFunction() {
@@ -58,7 +69,7 @@ function myFunction() {
 
 clearBtn.addEventListener("click", clearHistory)
 function clearHistory() {
-    document.getElementById("saved-list").innerHTML = ""
+    document.getElementById("saved-list").innerHTML = "";
 }
 
 
